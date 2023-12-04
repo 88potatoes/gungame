a2873287b38dec6e74a8d2f415cda43324c8fade
88potatoes <ericll360@gmail.com>
2023-12-05 09:44:47 +1100

    character moves smoothly

--- gungame/gungamecontroller.js
+++ gungame/gungamecontroller.js
@@ -2,36 +2,82 @@ const { sendJSON } = require('../ws-helpers')
 const info = require('../info.json')
 const websocket = new WebSocket(`ws://${info.ip_address}:8082`)
 
+let goup = false;
+let godown = false;
+let goright = false;
+let goleft = false;
+
 websocket.onopen = () => {
     sendJSON(websocket, {command: 'phone_join'})
     console.log('phone_join event')
 }
 
 const upbutton = document.getElementById('upbutton');
-upbutton.addEventListener('click', (e) => {
+upbutton.addEventListener('touchstart', (e) => {
     e.preventDefault();
-    sendJSON(websocket, {command: 'move-up'})
+    goup = true;
+})
+upbutton.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    goup = false;
 })
 
+
 const downbutton = document.getElementById('downbutton');
-downbutton.addEventListener('click', (e) => {
-    sendJSON(websocket, {command: 'move-down'})
+downbutton.addEventListener('touchstart', (e) => {
     e.preventDefault();
+    godown = true;
+})
+downbutton.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    godown = false;
 })
 
 const leftbutton = document.getElementById('leftbutton');
-leftbutton.addEventListener('click', (e) => {
+leftbutton.addEventListener('touchstart', (e) => {
     e.preventDefault();
-    sendJSON(websocket, {command: 'move-left'})
+    goleft = true;
+})
+leftbutton.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    goleft = false;
 })
 
 const rightbutton = document.getElementById('rightbutton');
-rightbutton.addEventListener('click', (e) => {
-    sendJSON(websocket, {command: 'move-right'})
+rightbutton.addEventListener('touchstart', (e) => {
     e.preventDefault();
+    goright = true;
+})
+rightbutton.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    goright = false;
 })
 
 // prevent double click zoom on phone
 document.ondblclick = function(e) {
     e.preventDefault()
+}
+
+const loop = () => {
+    update();
+    // console.log('looping')
+    // render();
+    requestAnimationFrame(loop)
+};
+
+window.requestAnimationFrame(loop)
+
+function update() {
+    if (goup) {
+        sendJSON(websocket, {command: "move-up"})
+    }
+    if (godown) {
+        sendJSON(websocket, {command: "move-down"})
+    }
+    if (goleft) {
+        sendJSON(websocket, {command: "move-left"})
+    }
+    if (goright) {
+        sendJSON(websocket, {command: "move-right"})
+    }
 }
